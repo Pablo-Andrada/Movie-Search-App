64b33524780f5690e387d8dada45b0460fc55fe8
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-08-27 20:10:20 -0300

    hooks y useFetch.

---
+++ Movie-Search-App/src/hooks/useFetch.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+const API_ENDPOINT = `https://www.omdbapi.com/?apikey=3c327a9b`;
+
+export const useFetch = params => {
+    const [isLoading,setIsLoading] = useState(true);
+    const [error,setError] = useState(false);
+    const [data, setData] = useState(null);
+    
+    const fetchMovie = url => {
+        setIsLoading(true);
+        fetch(url)
+            .then(respuesta => respuesta.json())
+            .then(respuestaJason => {
+                console.log("data: ", respuestaJason);            
+            })
+            .catch(error=>{console.log(error);
+        })     
+    }
+    useEffect(() => {
+        fetchMovie(`${API_ENDPOINT}${params}`)
+    },[params])
+    return { isLoading,error,data }
+}
