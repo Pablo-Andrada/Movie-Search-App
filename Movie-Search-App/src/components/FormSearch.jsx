08cba69cad1c07f0e23b6d6210560a61843e4d6b
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-08-26 22:01:22 -0300

    Algunos estilos

--- Movie-Search-App/src/components/FormSearch.jsx
+++ Movie-Search-App/src/components/FormSearch.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+
 const FormSearch = () => {
+
+    const [title, setTitle] = useState("")
+    
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log("title :", title);
+        
+    }
+
     return ( 
         <div className="form-search">
             <h2>Old Movies Finder</h2>
-            <form action="">
-                <input type="text" placeholder="Titulo de la pelicula" />
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Titulo de la pelicula" onChange={e=>setTitle(e.target.value)} />
                 <input type="submit" value="Search" />
             </form>
         </div>
